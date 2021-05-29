ca839683c43dc27dee9250567fffe8ed2c2e1fd2
Bulat Faizrakhmanov <58148408+btfv@users.noreply.github.com>
2021-05-29 18:16:22 +0300

    добавил спрайты

--- 5task_2/5task_2.cpp
+++ 5task_2/5task_2.cpp
@@ -4,6 +4,7 @@
 #include <SDL/SDL_image.h>
 #include <cstdlib>
 #include <ctime>
+#include <Windows.h>
 
 const int WIDTH = 600, HEIGHT = 600;
 const int framesPerMove = 30;
@@ -19,6 +20,7 @@ bool checkCollision(SDL_Rect& left, SDL_Rect& right) {
 
 int main(int argc, char* argv[])
 {
+    ShowWindow(GetConsoleWindow(), SW_HIDE);
     std::srand(std::time(nullptr));
     SDL_Init(SDL_INIT_EVERYTHING);
     SDL_Window* window = SDL_CreateWindow("5task_2", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, SDL_WINDOW_ALLOW_HIGHDPI);
@@ -27,7 +29,7 @@ int main(int argc, char* argv[])
     SDL_Surface* hedgehog = IMG_Load("sprites/sprite1.png");
     SDL_Surface* apple = IMG_Load("sprites/sprite2.png");
     if (!hedgehog || !apple) {
-        std::cout << "Image not found!";
+        std::cout << "Картинка не найдена!";
         exit(EXIT_FAILURE);
     }
     
@@ -55,8 +57,8 @@ int main(int argc, char* argv[])
 
         if (framesCounter == 0) {            
             for (int i = 0; i < apples.size(); i++) {
-                apples[i].position.y++;
-                apples[i].rotation++;
+                    apples[i].position.y++;
+                    apples[i].rotation++;
                 if (checkCollision(apples[i].position, hedgehogPosition) || apples[i].position.y > 580) {
                     stopHedgehog = true;
                     apples.erase(apples.begin() + i);
@@ -94,6 +96,9 @@ int main(int argc, char* argv[])
                     appleData newApple;
                     newApple.position.x += rand() % 400;
                     apples.push_back(newApple);
+                }
+                if (event.key.keysym.sym == SDLK_ESCAPE) {
+                    done = SDL_TRUE;
                 }
                 break;
             }
